6d5f95fd6acc2cd7e50363be3d5ced9e5a7cc356
DESKTOP-05Q6GBI\Nadine <wi21b045@technikum-wien.at>
2022-05-19 20:26:19 +0200

    Initial commit

---
+++ src/main/java/at/pistor/sumcalc/SumCalcApplication.java
@@ -0,0 +1,13 @@
+package at.pistor.sumcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/pistor/sumcalc/SumCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package at.pistor.sumcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
